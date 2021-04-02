588869899e556b5b296fb981c6d3e3d7df2f2002
dkdud8140 <cho.ay4@gmail.com>
2021-04-02 09:25:27 +0900

    OneDayProject

---
+++ Java_OneDay10_Score/src/com/callor/score/model/GwangJuScoreVO.java
@@ -0,0 +1,13 @@
+package com.callor.score.model;
+
+public class GwangJuScoreVO {
+
+	private String stuName ;
+	private int scoreKor ;
+	private int scoreEng ;
+	private int scoreMath ;
+	private int scoreSci ;
+	private int scoreHis ;
+
+	
+}
---
+++ Java_OneDay10_Score/src/com/callor/score/service/GwangJuScoreService.java
@@ -0,0 +1,23 @@
+package com.callor.score.service;
+
+import java.util.Scanner;
+
+public class GwangJuScoreService {
+
+	Scanner scan ;
+	
+	public GwangJuScoreService() {
+
+	
+	}
+
+	public void inputName() {
+		
+	}
+	
+	public void inputScore() {
+		
+		
+	}
+	
+}
